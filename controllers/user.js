5d1a43ad180dde0b2e94d372a92ae383a6dd9c22
Arpit <arpit@interfacem.com>
2020-08-31 01:44:04 +0530

    - Signin, added jwt to generate signed token and express-jwt for authorization check

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken'); // to generate signed token
+const expressJwt = require('express-jwt'); // for authorization check
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.signup = (req, res) => {
@@ -7,7 +9,7 @@ exports.signup = (req, res) => {
     user.save((err, user) => {
         if (err){
             return res.status(400).json({
-                err: errorHandler(err)
+                error: errorHandler(err)
             });
         }
 
@@ -18,5 +20,34 @@ exports.signup = (req, res) => {
         res.json({
             user
         });
+    })
+};
+
+exports.signin = (req, res) => {
+    // find the user based on email
+    const { email, password } = req.body;
+    User.findOne({email}, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User with this email does not exist.'
+            })
+        }
+        // if user exists, check authentication
+        // create authenticate method in user model
+        if (!user.authenticate(password)){
+            return res.status(401).json({
+                error: "Email and password don't match"
+            })
+        }
+
+        // generate a signed token with user id and secret
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET)
+
+        // persist the token as 't' in cookie with expiry date
+        res.cookie('t', token, {expire: new Date() + 9999})
+
+        // return response with user and token to frontend client
+        const { _id, name, email, role } = user
+        return res.json({token, user:{_id, name, email, role}})
     })
 };
--- models/user.js
+++ models/user.js
@@ -56,6 +56,11 @@ userSchema.virtual('password')
 
 // Adding encryptPassword method to userSchema
 userSchema.methods = {
+
+    authenticate: function(plainText){
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+
     encryptPassword: function (password) {
         if (!password) return '';
 
