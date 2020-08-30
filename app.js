45d9ce95440536015093fc1c1720bf44d7a90d40
Arpit <arpit@interfacem.com>
2020-08-31 00:39:45 +0530

    Signup validation & error handling using express validator

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 require('dotenv').config();
 
 // import routes
@@ -25,6 +26,7 @@ mongoose.connect(process.env.DATABASE, {
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
 
 // Routes middleware
 app.use("/api", userRoutes);
--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.signup = (req, res) => {
     console.log('req.body',req.body);
@@ -6,9 +7,14 @@ exports.signup = (req, res) => {
     user.save((err, user) => {
         if (err){
             return res.status(400).json({
-                err
-            })
+                err: errorHandler(err)
+            });
         }
+
+        // To hide the salt and hashed password in response
+        user.salt = undefined;
+        user.hashed_password = undefined;
+
         res.json({
             user
         });
