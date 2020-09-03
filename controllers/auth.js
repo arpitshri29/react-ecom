136c39863045075666c819eb8cd0fa80d895ef55
Arpit <arpit@interfacem.com>
2020-09-03 17:38:13 +0530

    - Auth & Admin middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -62,3 +62,29 @@ exports.requireSignin = expressJwt({
     algorithms: ["HS256"],
     userProperty: "auth"
 });
+
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+
+    if (!user){
+        return res.status(403).json({
+            error: "Access denied"
+        });
+    }
+
+    next();
+};
+
+
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0){
+        return res.status(403).json({
+            error: "Not an Admin! Access denied."
+        })
+    }
+
+    next();
+};
+
+
+
--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { requireSignin } = require('../controllers/auth');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 
 const { userById } = require('../controllers/user');
 
 router.param('userId', userById);
 
-router.get('/secret/:userId', requireSignin, (req, res) => {
+router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
     res.json({
         user: req.profile
     })
